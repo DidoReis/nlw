6882d74b3a4be4963393e23e1c7a8567f10b530f
DidoReis <djdidoreis@gmail.com>
2022-09-17 18:56:04 -0300

    NLW Esports - Rocketseat

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx", "./index.html"],
+  theme: {
+    fontFamily: {
+      sans: ["inter", "sans-serif"],
+    },
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        "nlw-gradient":
+          "linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 55.94%, #E1D55D 33.75%)",
+        "game-gradient":
+          "linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.9) 64.08%)",
+      },
+    },
+  },
+  plugins: [],
+};
